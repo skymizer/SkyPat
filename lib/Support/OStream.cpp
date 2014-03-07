ae21d7fad706208a2186922a3b6d83a791ec9bfa
Peter Chang <peter@skymizer.com>
2014-03-07 14:20:23 +0800

    Separate the declaration of OStream from pat.cpp

---
+++ lib/Support/OStream.cpp
@@ -0,0 +1,9 @@
+//===- OStream.cpp --------------------------------------------------------===//
+//
+//                     The pat Team
+//
+// This file is distributed under the New BSD License. 
+// See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+
