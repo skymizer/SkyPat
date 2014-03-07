f0eba835a6dcec3467f807081e253418df79ff38
Luba Tang <lubatang@gmail.com>
2014-03-07 17:10:35 +0800

    Separate Color.cpp from Color.h.

---
+++ lib/ADT/Color.cpp
@@ -0,0 +1,54 @@
+//===- Color.cpp ------------------------------------------------------===//
+//
+//                     The pat Team
+//
+// This file is distributed under the New BSD License. 
+// See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+#include "ADT/Color.h"
+
+using namespace pat;
+
+//===----------------------------------------------------------------------===//
+// Color
+//===----------------------------------------------------------------------===//
+Color::Color(kColor pColor, kType pType)
+  : m_Color(pColor), m_Type(pType) {
+}
+
+Color Color::Bold(kColor pColor)
+{
+  return Color(pColor, BOLD);
+}
+
+Color Color::BackGround(kColor pColor)
+{
+  return Color(pColor, BACKGROUND);
+}
+
+void Color::setColor(std::ostream& pOS)
+{
+  pOS << "\033[";
+  if (m_Color != RESET) {
+    if (m_Type == BOLD) {
+      pOS << "1;";
+    }
+    // TODO: Turn off bold mode;
+    int base_color = m_Type == BACKGROUND ? 40 : 30;
+    pOS << (base_color + m_Color);
+  }
+  pOS << 'm';
+}
+
+std::ostream& std::operator<<(std::ostream& pOS, Color pColor)
+{
+  pColor.setColor(pOS);
+  return pOS;
+}
+
+std::ostream& std::operator<<(std::ostream& pOS, Color::kColor pColor)
+{
+  Color(pColor).setColor(pOS);
+  return pOS;
+}
