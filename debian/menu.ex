0bc80f50ff85e3820048d0bfaa7588ac20aab5b3
Peter <peter@skymizer.com>
2018-04-08 14:52:45 +0800

    Remove the debian folder because our debian partner will help us rewrite it.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pat):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pat" command="/usr/bin/pat"
--- debian/pat.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pat package
-#
-0 4	* * *	root	[ -x /usr/bin/pat_maintenance ] && /usr/bin/pat_maintenance
